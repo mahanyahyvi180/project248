f5e6257ca99e6c57d0a6ad89b9943b4d39a55f75
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-30 20:58:02 +0330

    Add files via upload

---
+++ WindowsFormsApp120/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp120
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MyArray u = new MyArray();
+            foreach (object i in u)
+                listBox1.Items.Add(i);
+        }
+    }
+}
---
+++ WindowsFormsApp120/Interface1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp120
+{
+   public interface IEmployee
+    { 
+
+        string FirstName
+        {
+            get;
+            set;
+        }
+
+        string LastName
+        {
+            get;
+            set;
+        }
+
+        string Concatinate();
+
+
+    
+    }
+}
